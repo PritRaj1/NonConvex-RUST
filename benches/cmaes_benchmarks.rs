be16d49351a0959bfc03c90df462f7e2144b8407
PritRaj1 <prithvi.raj1@outlook.com>
2025-04-22 22:21:44 -0700

    benchmarks

---
+++ benches/cmaes_benchmarks.rs
@@ -0,0 +1,62 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use nalgebra::{DMatrix, DVector};
+use non_convex_opt::NonConvexOpt;
+use non_convex_opt::utils::config::Config;
+use serde_json;
+use std::sync::LazyLock;
+
+mod common;
+use common::fcns::{KBF, KBFConstraints};
+
+static CONFIG_JSON: &str = r#"
+{
+    "opt_conf": {
+        "max_iter": 10,
+        "rtol": "1e-8",
+        "atol": "1e-8",
+        "rtol_max_iter_fraction": 1.0
+    },
+    "alg_conf": {
+        "CMAES": {
+            "initial_sigma": 1.0,
+            "population_size": 100
+        }
+    }
+}"#;
+
+static CONFIG: LazyLock<Config> = LazyLock::new(|| {
+    serde_json::from_str(CONFIG_JSON).unwrap()
+});
+
+fn bench_cmaes_unconstrained(c: &mut Criterion) {
+    c.bench_function("cmaes_unconstrained", |b| {
+        b.iter(|| {
+            let init_x = DVector::from_fn(2, |_,_| rand::random::<f64>() * 10.0);
+            let mut opt = NonConvexOpt::new(
+                CONFIG.clone(),
+                black_box(DMatrix::from_columns(&[init_x])),
+                KBF,
+                None::<KBFConstraints>,
+            );
+            opt.run()
+        })
+    });
+}
+
+fn bench_cmaes_constrained(c: &mut Criterion) {
+    c.bench_function("cmaes_constrained", |b| {
+        b.iter(|| {
+            let init_x = DVector::from_fn(2, |_,_| rand::random::<f64>() * 10.0);
+            let mut opt = NonConvexOpt::new(
+                CONFIG.clone(),
+                black_box(DMatrix::from_columns(&[init_x])),
+                KBF,
+                Some(KBFConstraints)
+            );
+            opt.run()
+        })
+    });
+}
+
+criterion_group!(benches, bench_cmaes_unconstrained, bench_cmaes_constrained);
+criterion_main!(benches); 
